da81cfe8d2c8e63222537b7cdcc97902911512c9
innnn.2 <109224080+innnn2@users.noreply.github.com>
2022-08-19 22:15:41 +0900

    Add files via upload

---
+++ python-tutorial/4_3_list.py
@@ -0,0 +1,15 @@
+student = ["egoing", "sori", "maru"]
+grades = [2,1,4]
+
+print(student[1])
+print(len(student))
+
+print(min(grades))
+print(max(grades))
+
+import statistics
+
+print(statistics.mean(grades))
+
+import random
+print(random.choice(student))
